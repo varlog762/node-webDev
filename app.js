390340a70dcc0145cec11a2a14ddb30dbf5c6563
Gregory Pereverzev <boot762@gmail.com>
2024-03-13 22:51:04 +0300

    feat: add lesson_04 code

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-const fs = require();
+const fs = require('fs');
+
+// const fileData = fs.readFileSync('./test.txt', 'utf-8');
+
+// console.log(fileData);
+
+// fs.readFile('./test.txt', 'utf-8', (err, data) => {
+//   fs.mkdir('./files', err => {
+//     if (!err) {
+//       fs.writeFile('./files/new-test.txt', data, err => {
+//         err ? console.log(err) : null;
+//       });
+//     }
+//   });
+// });
+
+fs.readFile('./test.txt', 'utf-8', (err, data) => {
+  //Don't use sync node.js methods!!!!!!
+  fs.mkdirSync('./files', () => {});
+  //Don't use sync node.js methods!!!!!!
+  fs.writeFileSync('./files/new-test.txt', data, err => {
+    err ? console.log(err) : null;
+  });
+});
+
+setTimeout(() => {
+  if (fs.existsSync('./files/new-test.txt')) {
+    fs.unlink('./files/new-test.txt', () => {});
+  }
+}, 4000);
+
+setTimeout(() => {
+  if (fs.existsSync('./files')) {
+    fs.rmdir('./files', () => {});
+  }
+}, 6000);
