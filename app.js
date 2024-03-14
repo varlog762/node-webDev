0eca0801163367cfc70b79b4c566e564185904bd
Gregory Pereverzev <boot762@gmail.com>
2024-03-14 18:01:04 +0300

    feat: add lesson_05 (Event Module) code

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-const fs = require('fs');
+const Logger = require('./log');
 
-// const fileData = fs.readFileSync('./test.txt', 'utf-8');
+const logger = new Logger();
 
-// console.log(fileData);
-
-// fs.readFile('./test.txt', 'utf-8', (err, data) => {
-//   fs.mkdir('./files', err => {
-//     if (!err) {
-//       fs.writeFile('./files/new-test.txt', data, err => {
-//         err ? console.log(err) : null;
-//       });
-//     }
-//   });
-// });
-
-fs.readFile('./test.txt', 'utf-8', (err, data) => {
-  //Don't use sync node.js methods!!!!!!
-  fs.mkdirSync('./files', () => {});
-  //Don't use sync node.js methods!!!!!!
-  fs.writeFileSync('./files/new-test.txt', data, err => {
-    err ? console.log(err) : null;
-  });
+logger.on('some_event', args => {
+  const { id, text } = args;
+  console.log(id, text);
 });
 
-setTimeout(() => {
-  if (fs.existsSync('./files/new-test.txt')) {
-    fs.unlink('./files/new-test.txt', () => {});
-  }
-}, 4000);
-
-setTimeout(() => {
-  if (fs.existsSync('./files')) {
-    fs.rmdir('./files', () => {});
-  }
-}, 6000);
+logger.log('User logged!');
